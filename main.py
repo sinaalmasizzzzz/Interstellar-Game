f079e13bafcd6c78403ee7625cd4a4dd317c5323
sinaalmasizzzzz <sinaalmasizzzzz@gmail.com>
2024-06-22 14:46:56 -0700

    ...

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import arcade
-#همپورت کردم کتابخانه را
+#متابخانه بازی سازس را ایمپورت کردیم
 class Game(arcade.Window):
+    #یک کلاس که زیر مجموعه ویندو در arcade است را ایجاد کردیم
     def __init__(self):
+        #یک تابع فعال در کلاس ساختیم
         super().__init__(width=600,height=600,title="game")
-        #یک پنجره باز کردم
+        #مشخصات یک پنجره را داده ایم
         arcade.set_background_color(arcade.color.BLUE)
+        #رنگ س زمینه را انتخاب کردیم
         self.background=arcade.load_texture("BG.png")
         #بک گراند را تنظیم کردم
     def on_draw(self):
+        #حالا یک تابع جدید ساختیم
         arcade.start_render()
+        #استارت کردیم رندر گرفتم را
         arcade.draw_lrwh_rectangle_textured(0,0,self.width,self.height,self.background)
+        #مشخصات پنجره را دادیم
         arcade.finish_render()
-    #حالا بازش کردم
+        #ان را تمام کردیم
+
 if __name__ == "__main__":
+    #یک شرط گذاشتیم
     window=Game()
+    #و ویندو را مساوی با کلاس گیم کردیم
     arcade.run()
-#پنجره را ران کردم
+    #حالا ان را ران کردین
